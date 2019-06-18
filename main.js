c0c59499ab438ec2dbf6311a26f95b6f0bc3e1c7
TodoListDiguinho <lucassarri@hotmail.com>
2019-06-18 19:47:34 -0300

    adicionando o js

---
+++ main.js
@@ -0,0 +1,16 @@
+function adicionarTarefa()
+{
+    //Aqui deve-se colocar o elemento no localstorage e renderizar o elemento na tela
+}
+function deletarTarefa()
+{
+    //Aqui deve-se deletar o elemento do localstorage e apagar da tela
+}
+function tarefaCompleta()
+{
+    //Aqui a tarefa ira desaparecer da tela, uma coisa a se pensar é chamar a função de deletar
+}
+function renderizar()
+{
+    //Aqui cria os elementos html a partir do adicionar tarefa e carrega na tela
+}
