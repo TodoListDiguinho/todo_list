3025341ff72e17b04ca534981d75c19beef1d3dc
lucasSarri <lucassarri@hotmail.com>
2019-06-21 10:12:28 -0300

    alteracoes dia 21/06

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
+var tarefas = [];
+
+class Tarefa 
+{
+	constructor()
+	{
+		this.id;
+		this.desc;
+		this.feito;
+	}
+	entrada()
+	{
+		this.id=tarefas.length;
+		this.desc=document.getElementById('descTarefa').value;
+		this.feito = false;
+	}
+}
+
 function adicionarTarefa()
 {
     //Aqui deve-se colocar o elemento no localstorage e renderizar o elemento na tela
+    var tarefa = new Tarefa();
+    tarefa.entrada(); 
+    console.log(tarefa);
+    tarefas.push(tarefa);
+    console.log(tarefas);
+
 }
 function deletarTarefa()
 {
@@ -13,4 +37,12 @@ function tarefaCompleta()
 function renderizar()
 {
     //Aqui cria os elementos html a partir do adicionar tarefa e carrega na tela
+    //<li>
+    //            <div class="itemDiv" onclick="tarefaCompleta()">
+   //             	Comer Pão  <i class="material-icons" onclick="deletarTarefa()">delete</i>   
+   //             </div>
+	//</li>
+	var li = document.createElement('li');
+	var div = document.createElement('div');
+	
 }
